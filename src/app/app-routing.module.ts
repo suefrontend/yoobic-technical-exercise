d1c062969bb285828e5128243dc19733d851dc82
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-11-06 01:06:08 -0700

    feat: add routing for masterlist-detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { Tab3Page } from './tab3/tab3.page';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'masterlist-detail/:id', component: Tab3Page },
 ];
 @NgModule({
   imports: [
