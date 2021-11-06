a1d902fb55a2a5bbecd3d59fdbbb268bed00b61f
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-11-06 15:49:54 -0700

    refactor: fix component name in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { SignIn } from './signin/signin.page';
-import { MasterList } from './masterlist/masterlist.page';
-import { MasterListDetail } from './masterlist-detail/masterlist-detail.page';
+import { MasterListPage } from './masterlist/masterlist.page';
+import { MasterListDetailPage } from './masterlist-detail/masterlist-detail.page';
 
 const routes: Routes = [
   { path: '', redirectTo: 'signin', pathMatch: 'full' },
   { path: 'signin', component: SignIn },
-  { path: 'masterlist', component: MasterList },
-  { path: 'masterlist-detail/:id', component: MasterListDetail },
+  { path: 'masterlist', component: MasterListPage },
+  { path: 'masterlist-detail/:id', component: MasterListDetailPage },
 ];
 @NgModule({
   imports: [
