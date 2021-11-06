854115e8ad04c8799fafde062c3cdbbb7dc95f7a
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-11-06 15:36:48 -0700

    refactor: fix/add routers in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { Tab3Page } from './tab3/tab3.page';
+import { SignIn } from './signin/signin.page';
+import { MasterList } from './masterlist/masterlist.page';
+import { MasterListDetail } from './masterlist-detail/masterlist-detail.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  { path: 'masterlist-detail/:id', component: Tab3Page },
+  { path: '', redirectTo: 'signin', pathMatch: 'full' },
+  { path: 'signin', component: SignIn },
+  { path: 'masterlist', component: MasterList },
+  { path: 'masterlist-detail/:id', component: MasterListDetail },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
