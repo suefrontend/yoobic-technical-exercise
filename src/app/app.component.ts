8f87cdfdeefb1a68fce0d6deb7092d849f0c0656
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-11-06 05:01:04 -0700

    feat: create side menu and make it accessable from every page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  public pages = [
+    { title: 'Master List', url: '/masterlist', icon: 'mail' },
+    { title: 'Sign In', url: '/signin', icon: 'paper-plane' },
+    { title: 'Stencil Component', url: '/stencil', icon: 'heart' },
+    { title: 'Link', url: '#', icon: 'archive' },
+    { title: 'Link', url: '#', icon: 'trash' },
+    { title: 'Link', url: '#', icon: 'warning' },
+  ];
+
   constructor() {}
 }
