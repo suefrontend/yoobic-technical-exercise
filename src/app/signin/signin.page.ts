06a62d470f97b04bc6ece56b3ab63057db5db7e4
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-11-06 17:45:15 -0700

    feat: set a link to master list page on sign-in button

--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['signin.page.scss'],
 })
 export class SignIn {
-  constructor() {}
+  constructor(private route: Router) {}
+
+  signin() {
+    this.route.navigate(['/masterlist']);
+  }
 }
