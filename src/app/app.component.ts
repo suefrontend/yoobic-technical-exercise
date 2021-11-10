03fa34f98ffc7665042f20f3c9bcbeaac4a420e9
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-11-09 16:36:13 -0800

    refactor: remove unnecessary code from slide-in navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   public pages = [
     { title: 'Master List', url: '/masterlist', icon: 'mail' },
     { title: 'Sign In', url: '/signin', icon: 'paper-plane' },
-    { title: 'Stencil Component', url: '/stencil', icon: 'heart' },
+    { title: 'Link', url: '#', icon: 'heart' },
     { title: 'Link', url: '#', icon: 'archive' },
     { title: 'Link', url: '#', icon: 'trash' },
     { title: 'Link', url: '#', icon: 'warning' },
